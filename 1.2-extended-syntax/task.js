9b71b9c2cd8fd28c90812466588cae79a5c11aba
Vera Koptelova <koptelova.ver@yandex.ru>
2020-02-07 18:24:38 +0500

    second_2 commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -10,9 +10,16 @@ function getResult(a,b,c){
     return x;
     }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+    if (marks.length > 5) {
+        console.log("Оценок больше 5-ти");
+        marks = marks.slice(0, 5);
+    }
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum = marks[i] + sum;
+    }
+    return sum / marks.length;
 }
 
 function askDrink(name,dateOfBirthday){
