52135dcc45f13d771a0a1c3666b89c2091fb7b6d
Vera Koptelova <koptelova.ver@yandex.ru>
2020-02-07 17:50:52 +0500

    first_2 commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,7 +1,14 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
-}
+    let x = [];
+    let d = Math.pow(b, 2) - 4 * a * c;
+    if (d === 0) {
+        x.push((-b + Math.sqrt(d)) / 2 * a);
+    } else if (d > 0) {
+        x.push((-b + Math.sqrt(d)) / 2 * a);
+        x.push((-b - Math.sqrt(d)) / 2 * a);
+    }
+    return x;
+    }
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
