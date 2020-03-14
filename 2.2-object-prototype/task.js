9dd5fa8483b47c156af5a45b33440547a3039f81
Vera Koptelova <koptelova.ver@yandex.ru>
2020-03-14 14:06:18 +0500

    object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,30 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    let sound = animal.sound;
+    if (animal == undefined){
+        return null
+    } else {
+        return sound
+    } 
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        let parseMarks = parseInt(marks[i]);
+        sum = sum + parseMarks;
+    }
+    let average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+    
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let ago = new Date(birthday);
+    let now = new Date();
+    let needed = new Date(now.setFullYear(now.getFullYear() - 18));
+    if (needed < ago) {
+        return false;
+    }
+    return true;
 }
